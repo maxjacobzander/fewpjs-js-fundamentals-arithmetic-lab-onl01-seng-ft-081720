aea887f4d772a022cb5e0388adf0797af256f080
maxjacobzander <maxjacobzander@gmail.com>
2020-12-05 10:13:15 -0600

    commit tree 8f0d27ed2880906cd282ed3a201d76359f308c41

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+
+const num1 = 31;
+const num2 = 2;
+
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * 10) + 1;
+
+const mod = 16 % 6;
+
+const max = Math.max(1,2,5,8,18,20);
